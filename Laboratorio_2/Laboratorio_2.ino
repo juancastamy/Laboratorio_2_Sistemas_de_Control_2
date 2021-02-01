335305e2657c6f42d0f1874300cc5b93f0f3f876
juancastamy <45299603+juancastamy@users.noreply.github.com>
2021-02-01 08:13:47 -0600

    inicio de proyecto

---
+++ Laboratorio_2/Laboratorio_2.ino
@@ -0,0 +1,11 @@
+void setup() {
+  pinMode(PF_4,INPUT};
+  pinMode(PF_0,INPUT};
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly: 
+  
+}
